a122a26c3d4201b064ecbce094f9528bb6592a1c
kosychsa <kosych.sascha@gmail.com>
2024-11-08 10:42:53 +0100

    Caesar

---
+++ src/Caesar.java
@@ -0,0 +1,42 @@
+public class Caesar {
+
+    private String Kt;
+    private int S;
+    private String Gt;
+
+
+        public void caesar() {
+
+        }
+
+        public void verschluesseln(){
+
+        }
+
+        public void entschluessel() {
+
+        }
+
+        public int getASCII(char pWert) {
+            return (int) pWert;
+        }
+
+        public char getChar(int pWert) {
+            return (char) pWert;
+        }
+
+        public void getKt(String pWert) {
+
+        }
+
+        public String getKt() {
+            return (String) Kt;
+        }
+
+        public void getS(int pWert) {
+
+        }
+
+
+    }
+
