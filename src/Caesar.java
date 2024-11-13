58c9f4e485b9c7034a5b8d8bdb4bb6db47466d0f
Soascha <kosych.sascha@gmail.com>
2024-11-13 11:34:49 +0100

    Update Caesar.java

--- src/Caesar.java
+++ src/Caesar.java
@@ -4,19 +4,22 @@ public class Caesar {
     private int S;
     private String Gt;
 
+    public Caesar(){}
 
-        public void caesar() {
-
-        }
 
         public void verschluesseln(){
-
+            for (int i = 0 ; i < Kt.length()  ; i++) {
+                int h = this.getASCII(Kt.charAt(i));
+                Gt  = Gt +  this.getChar(h + S);
+            }
         }
 
-        public void entschluessel() {
-
-        }
-
+        public void entschluesseln() {
+            for (int i = 0; i < Gt.length() ; i++) {
+                int h = this.getASCII(Gt.charAt(i));
+                Kt = Kt + this.getChar(h - S);
+            }
+    }
         public int getASCII(char pWert) {
             return (int) pWert;
         }
@@ -36,6 +39,21 @@ public class Caesar {
         public void getS(int pWert) {
 
         }
+        public String getGt() {
+            return (String) Gt;
+        }
+
+        public void setKt(String zeichenkette) {
+        }
+
+        public void setS(int schluessel) {
+        }
+
+
+
+}
+
+
 
 
     }
